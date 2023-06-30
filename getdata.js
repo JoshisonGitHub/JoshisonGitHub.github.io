9fddf371960ff142b37461ce900526467e069521
JoshisonGitHub <joshua.d.pulham@durham.ac.uk>
2023-06-30 15:38:24 +0200

    add getdata.js

---
+++ getdata.js
@@ -0,0 +1,38 @@
+// loadJSON method to open the JSON file.
+function loadJSON(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          success(JSON.parse(xhr.responseText));
+        }
+        else {
+          error(xhr);
+        }
+      }
+    };
+    xhr.open('GET', path, true);
+    xhr.send();
+  }
+  
+  loadJSON("https://docs.google.com/spreadsheets/d/e/2PACX-1vScByvw0FSA_1Loy83T8r60ahM-EAtzL5bRvfCpxFOMj7Gp562E9WNZzQ4u4HVkJrzC2e6tI20ojlY1/pubhtml", myData,'jsonp');
+  
+  function myData(Data)
+  {
+  
+    // Output only the details on the first post
+    console.log(Data[0]);
+  
+    // output the details of first three posts
+    console.log("First three posts");
+    for(var i=0; i<3; i=i+1)
+    {
+      console.log(Data[i]);
+    } 
+    // output the id field of first five elements. 
+    console.log("First five ID");
+    for(var i=0; i<5; i=i+1)
+    {
+      console.log(Data[i].id);
+    }
+  }
